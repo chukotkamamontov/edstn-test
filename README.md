7d5bc4ad7dbf574610d6191ef155a89a4beb8601
Sergey Kharitonov <kharitonov.si@gmail.com>
2023-02-03 14:52:11 +0400

    installed cra-ts

--- README.md
+++
@@ -1,1 +0,0 @@
-# edstn-test
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './assets/css/app.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      content will be here
+    </div>
+  )
+}
+
+export default App
